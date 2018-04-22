f24a37602c49fefa292d8c50871a9733c583a2fe
christos <chrisperis@gmail.com>
2018-04-22 15:07:47 +0300

    bean-scope-demo Adds structure for demo

---
+++ bean-scope-demo/src/test/java/com/codehub/spring/beanscopedemo/BeanScopeDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codehub.spring.beanscopedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeanScopeDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
